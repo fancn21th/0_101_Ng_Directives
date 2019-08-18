8e8797a348c848b47be5a5e10e84785338a758c2
fancn <fanyj@yikeer.com>
2019-08-18 13:41:20 +0800

    rename

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { FirstDirectiveDirective } from "./directives/first-directive.directive";
-import { SecondDirectiveDirective } from "./directives/second-directive.directive";
+import { FirstDirectiveDirective } from "./directives/first.directive";
+import { SecondDirectiveDirective } from "./directives/second.directive";
 import { HomeComponent } from "./components/home/home.component";
 import { TrackDirective } from "./directives/track.directive";
 import { TrackingService } from "./services/tracking.service";
