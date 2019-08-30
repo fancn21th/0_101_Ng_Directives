1833695d34180980e1e66057a2f667d73fdaccd0
alex.fan <alex.fan@okchem.com>
2019-08-30 15:35:36 +0800

    update second directive's comment

--- src/app/directives/second.directive.ts
+++ src/app/directives/second.directive.ts
@@ -4,8 +4,9 @@ import { Directive, HostBinding, Input, HostListener } from "@angular/core";
   selector: "[second]"
 })
 export class SecondDirective {
-  @Input() second;
+  @Input() second; // 1st take input from host
   @HostBinding() get innerText() {
+    // 2nd update host based on input
     return `from directive ${this.second}`;
   }
   @HostListener("click", ["$event"]) onclick($event) {
